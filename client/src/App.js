8295787d6a2b4846486daa570cd32b13fc8970ee
BeelzeBub2809 <nguyenanhtuan28092003@gmail.com>
2024-06-13 02:53:18 +0700

    Fix import react-router-dom and add landing page again

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import PageNotFound from './app/error/page-not-found';
 import DefaultLayoutAdmin from './app/shared/layouts/admin/default-layout.admin'
+import LandingPage from './app/shared/layouts/common/landingPage';
 function App() {
   return (
     <Router>
       <div className='App'>
         <Routes>
+          <Route path='/' element = {<LandingPage/>} />
           <Route path='/*' element={<PageNotFound/>} />
           <Route path = 'manager/*' element = {< DefaultLayoutAdmin/>} />
         </Routes>
