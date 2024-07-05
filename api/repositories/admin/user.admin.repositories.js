5e0a219aabb474562b101ab6b3137d7f08a1ae27
BeelzeBub2809 <nguyenanhtuan28092003@gmail.com>
2024-07-05 08:20:48 +0700

    Update API list/detail/update user admin role

--- api/repositories/admin/user.admin.repositories.js
+++ api/repositories/admin/user.admin.repositories.js
@@ -19,7 +19,62 @@ async function createUser({ email, password, fullname, phone, avatar, status, ro
   const newUser = await DbUser.create(User)
   return newUser
 }
+async function getAllUser({ page, size, searchString }) {
+  let query = {}
+  if (searchString) {
+    query = {
+      $or: [
+        { email: { $regex: `.*${searchString}.*`, $options: 'i' } },
+        { fullname: { $regex: `.*${searchString}.*`, $options: 'i' } },
+        { phone: { $regex: `.*${searchString}.*`, $options: 'i' } }
+      ]
+    };
+  }
+  const users = await DbUser.find(query)
+    .skip((page - 1) * size)
+    .limit(size)
+    .populate('roles');
+  const filteredUsers = users.map(user => ({
+    ...user.toObject(),
+    roles: user.roles.map(role => role.name)
+  }))
+  return filteredUsers
+}
+async function getDetailUser(userId) {
+  const user = await DbUser.findById(userId).populate('roles')
+  if (!user) {
+    return null;
+  }
+  const mappedUser = {
+    ...user.toObject(),
+    roles: user.roles.map(role => role.name)
+  }
+  return mappedUser
+}
+async function updateUser({id, email, password, fullname, phone, avatar, status, roles }) {
+  const roleObjs = await DbRole.find({ name: { $in: roles } }).exec()
+  const roleIds = roleObjs.map(r => r._id.toString())
+  console.log(roleIds);
+  const updatedUser = await DbUser.findByIdAndUpdate(
+    id,
+    {
+      $set: {
+        email: email,
+        password: password,
+        fullname: fullname,
+        phone: phone,
+        avatar: avatar,
+        status: status,
+      },
+      $addToSet: {
+        roles: { $each: roleIds}
+      }
+    },
+    {new: true, runValidators: true}
+  )
+  return updatedUser
+}
 const userAdminRepo = {
-  createUser
-} 
+  createUser, getAllUser, getDetailUser, updateUser
+}
 module.exports = userAdminRepo
