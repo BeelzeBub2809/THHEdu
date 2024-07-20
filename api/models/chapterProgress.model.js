7369dfe553f949c267ac05d3a5f4963817326c3e
NgTuanHung0511 <hocfronendvoinabatiicheese@gmail.com>
2024-07-20 13:37:16 +0700

    Update database
    (cherry picked from commit 6eac4a1c241c328fdd56d666a33aaf3f56db5f39)
    (cherry picked from commit 8104ece8493a17637f2f276fbd463d645bf61424)

---
+++ api/models/chapterProgress.model.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+const chapterProgressSchema = new Schema({
+  trainee: {
+    type: Schema.ObjectId,
+    ref: 'User',
+  },
+  chapter: {
+    type: Schema.ObjectId,
+    ref: 'Chapter',
+  },
+  isCompleted: {
+    type: Boolean,
+    default: false,
+  },
+  completedQuizzes: [{
+    submittedQuiz: {
+      type: Schema.ObjectId,
+      ref: 'SubmittedQuiz',
+    },
+  }],
+  videoProgress: {
+    type: Number,
+    default: 0
+  }
+},{
+  timestamps: true
+})
+const DbChapterProgress = mongoose.model('ChapterProgress', chapterProgressSchema)
+module.exports = DbChapterProgress
