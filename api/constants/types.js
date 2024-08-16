1fe102c3f13b7fedc0394b7a3e8ff392be413528
NgTuanHung0511 <hocfronendvoinabatiicheese@gmail.com>
2024-08-16 14:43:52 +0700

    4

--- api/constants/types.js
+++ api/constants/types.js
@@ -7,7 +7,10 @@ const chapterType = {
 const questionType = {
     MCQ: 'MCQ', // multiple choice question,
     MAQ: 'MAQ', // multiple answer question,
-    BOOLEAN: 'boolean' // true/false question
+    BOOLEAN: 'boolean', // true/false question,
+    BOOLEAN1: 'boolean', // true/false question,
+    BOOLEAN2: 'boolean',
+    BOOLEAN3: 'boolean',
 }
 
 module.exports = { chapterType, questionType }
